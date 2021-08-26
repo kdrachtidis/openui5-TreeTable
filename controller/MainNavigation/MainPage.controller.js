0fbb377baf4a37c0e420f5c932f967d38b89bed6
Konstantinos Drachtidis <konstantinos.drachtidis@sap.com>
2021-08-26 11:53:00 +0200

    INit

--- controller/MainNavigation/MainPage.controller.js
+++ controller/MainNavigation/MainPage.controller.js
@@ -1,9 +1,15 @@
 sap.ui.define([
-    'sap/ui/core/mvc/Controller'
-], function (Controller) {
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/model/json/JSONModel'
+], function (Controller, JSONModel) {
     "use strict";
 
-    var Controller = Controller.extend("DemoUXapp.controller.MainNavigation.MainPage");
+    var Controller = Controller.extend("DemoUXapp.controller.MainNavigation.MainPage", {
+        onInit: function() {
+			var oModel = new JSONModel("localservice/mockdata/clothing.json");
+			this.getView().setModel(oModel);
+		}
+    });
 
     return Controller;
 });
